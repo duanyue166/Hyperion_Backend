5a50c471e2d797eda3a63b0e86bd90ffa85286a6
dy <imduanyue@qq.com>
2024-09-20 11:15:09 +0800

    Initial comment

---
+++ src/test/java/org/hydra/hyperion_backend/HyperionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hydra.hyperion_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
